bd88772bbd8d9fbaab531540ab01379efc327981
Brayan-Schmitz <brayan.schmitz@univille.br>
2026-03-18 19:41:13 -0300

    brayan-schmitz

---
+++ univille/Semestre 1/APC_I/aula004/ex001.py
@@ -0,0 +1,5 @@
+A = float(input("Digite um valor: "))
+B = float(input("Digite um valor: "))
+soma = A + B
+if soma > 10:
+    print("A soma desses valores é {}".format(soma))
---
+++ univille/Semestre 1/APC_I/aula004/ex002.py
@@ -0,0 +1,8 @@
+A = float(input("Digite um valor: "))
+B = float(input("Digite um valor: "))
+soma = A + B
+if soma >= 10:
+    soma = soma + 5
+else:
+    soma = soma - 7
+print("A soma desses valores é {}".format(soma))
