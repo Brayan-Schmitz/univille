ed59797f0a8a57c9f54a6da05152182ca17ea630
Brayan-Schmitz <brayan.schmitz@univille.br>
2026-03-25 17:56:56 -0300

    brayan-schmitz

--- univille/Semestre 1/APC_I/aula005-2/ex007.py
+++ univille/Semestre 1/APC_I/aula005-2/ex007.py
@@ -6,7 +6,7 @@ notacg = float(input("Digite a nota de Conhecimentos Gerais: "))
 media = (notap + notam + notacg) / 3
 
 print("O candidato, {}, tem as notas: \nNota de Português {} \nNota de Matemática {} \nNota de Conhecimentos Gerais {}". format(name, notap, notam, notacg))
-print("A média do candidato, {}, é {}".format(name, media))
+print("A média do candidato, {}, é {:.2f}".format(name, media))
 
 if (media >= 7 and notap > 5 and notam > 5 and notacg > 5):
     print(f"O candidato, {name}, foi aprovado")
