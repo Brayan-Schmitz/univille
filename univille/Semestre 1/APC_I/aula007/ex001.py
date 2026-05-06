f70e0a5af21be93a1903e6efc3c9bbb80ad6888e
Brayan-Schmitz <brayan.schmitz@univille.br>
2026-05-06 20:27:39 -0300

    brayan-schmitz

---
+++ univille/Semestre 1/APC_I/aula007/ex001.py
@@ -0,0 +1,12 @@
+idades = []
+
+idade = int(input("Informe a idade do aluno: "))
+
+while idade >= 0:
+    idades.append(idade)
+    idade = int(input("Informe a idade do aluno: "))
+
+print(f"As idades digitadas foram: {idades}")
+print(f"O aluno mais novo possui: {min(idades)} anos")
+print(f"O aluno mais velho possui: {max(idades)} anos")
+print(f"A média de idade da sala é: {sum(idades) / len(idades) :.2f} anos")
