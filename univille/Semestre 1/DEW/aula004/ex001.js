e0aa119b62720481d2a9e1716cda696c7e9d66df
Brayan-Schmitz <brayan.schmitz@univille.br>
2026-06-23 21:56:38 -0300

    brayan-schmitz

---
+++ univille/Semestre 1/DEW/aula004/ex001.js
@@ -0,0 +1,44 @@
+class Produto {
+  constructor(nome, categoria, preco, estoque) {
+    this.nome = nome;
+    this.categoria = categoria;
+    this.preco = Number(preco);
+    this.estoque = estoque;
+  }
+
+  nivelEstoque() {
+    if (this.estoque < 5) {
+      return "baixo";
+    }
+
+    return "adequado";
+  }
+
+  saida() {
+    console.log(`Produto: ${this.nome}`);
+    console.log(`Categoria: ${this.categoria}`);
+    console.log(`Preço: R$ ${this.preco}`);
+    console.log(`Estoque: ${this.nivelEstoque()}`);
+    console.log();
+  }
+
+  visaoTela() {
+    console.log(`Produto: ${this.nome} (${typeof this.nome})`);
+    console.log(`Categoria: ${this.categoria} (${typeof this.categoria})`);
+    console.log(`Preço: ${this.preco} (${typeof this.preco})`);
+    console.log(
+      `Estoque: ${this.nivelEstoque()} (${typeof this.nivelEstoque()})`,
+    );
+    console.log();
+  }
+}
+
+const monitor = new Produto("Monitor", "Informática", "899.90", 5);
+
+monitor.saida();
+monitor.visaoTela();
+
+const cadeira = new Produto("Cadeira", "Escritório", "450.00", 30);
+
+cadeira.saida();
+cadeira.visaoTela();
