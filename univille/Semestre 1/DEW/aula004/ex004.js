eb02002ff5cdcc8bac39998eca0f5fe156d2e136
Brayan-Schmitz <brayan.schmitz@univille.br>
2026-06-26 15:06:33 -0300

    brayan-schmitz

---
+++ univille/Semestre 1/DEW/aula004/ex004.js
@@ -0,0 +1,25 @@
+const gastos = ["500", "1200", "abc", "350", "1000", ""];
+
+function calcularTotal(lista) {
+  let total = 0;
+
+  for (let i = 0; i < lista.length; i++) {
+    const valor = Number(lista[i]);
+
+    if (isNaN(valor) || lista[i] === "") continue;
+
+    total += valor;
+  }
+
+  return total;
+}
+
+const totalGastos = calcularTotal(gastos);
+
+console.log(`Total: R$ ${totalGastos}`);
+
+console.log(
+  totalGastos > 2000
+    ? "Limite ultrapassado"
+    : "Gastos dentro do limite",
+);
